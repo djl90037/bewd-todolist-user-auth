dd98ac1b057a7f6fbd81e160fbb35a2c7ab9c5ed
root <root@38ZPMG3.dirsvcs.org>
2022-10-11 14:04:45 -0500

    commit

--- db/migrate/20161215094136_add_attributes_to_tasks.rb
+++ db/migrate/20161215094136_add_attributes_to_tasks.rb
@@ -2,5 +2,6 @@ class AddAttributesToTasks < ActiveRecord::Migration[5.0]
   def change
     add_column :tasks, :content, :string
     add_column :tasks, :completed, :boolean, default: false
+    add_belongs_to :tasks, :user
   end
 end
